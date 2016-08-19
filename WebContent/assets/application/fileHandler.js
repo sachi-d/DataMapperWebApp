44a9ed9d5f31b1ac10e420837ed061edea135608
Sachithra Dangalla <sachithradangalla@gmail.com>
2016-08-19 12:12:18 +0530

    complete XML parsing

--- WebContent/assets/application/fileHandler.js
+++ WebContent/assets/application/fileHandler.js
@@ -1,41 +1,99 @@
 (function () {
 
-    // output information
-    function appendToResult(msg, result) {
-        var m = document.getElementById(result);
-        m.innerHTML = m.innerHTML + msg;
+    function setDivElement(nodeName) {
+        return "<div class=\"leaf\">" + nodeName + "</div>";
     }
 
+    function setInLeaf(nodeName) {
+        return "<div class=\"leaf in-leaf glyphicon glyphicon-play\">" + nodeName + "</div>";
+    }
+
+    function setOutLeaf(nodeName) {
+        return "<div class=\"leaf out-leaf glyphicon glyphicon-play\"> " + nodeName + "</div>";
+    }
+
+    //traverse XML tree
+    function traverseTree(rootNode, resultBox) {
+        if (rootNode.nodeType !== Node.ELEMENT_NODE) {
+            return;
+        }
+        var children = rootNode.childNodes;
+        var nodeName = "";
+        if (children.length == 1) {
+            if (resultBox.split("-")[0] == "input") {
+                nodeName = setInLeaf(rootNode.nodeName);
+            } else {
+                nodeName = setOutLeaf(rootNode.nodeName);
+            }
+        } else {
+            nodeName = setDivElement(rootNode.nodeName);
+        }
+        $("#" + resultBox).append(nodeName);
+        for (var i = 0; i < children.length; i++) {
+            var child = children[i];
+            traverseTree(child, resultBox);
+            if (child.nodeType == Node.ELEMENT_NODE && child.childNodes.length != 1) {
+                break;
+            }
+        }
+    }
+
+    //parse XML tree
+    function parseXMLTree(inputText, resultBox) {
+        parser = new DOMParser();
+
+
+        var xmlDoc = parser.parseFromString(inputText, "text/xml");
+
+        // documentElement always represents the root node
+        var root = xmlDoc.documentElement;
+        traverseTree(root, resultBox);
+
+    }
+
+
+    // get file information
+    function parseFile(file, resultpane) {
+        var fileContentPane = resultpane.split("-")[0] + "-file-content";
+        if (file.name.endsWith(".xml")) {
+            var res = "<p>File name: <strong>" + file.name + "</strong><br> type: <strong>" + file.type + "</strong><br> size: <strong>" + file.size + "</strong> bytes</p>";
+            $("#" + fileContentPane).append(res);
+
+            // display text
+            if (file.type.indexOf("text") == 0) {
+                var reader = new FileReader();
+                reader.onload = function (e) {
+                    var restxt = "<p><strong>File content:</strong></p><pre>" + e.target.result.replace(/</g, "&lt;").replace(/>/g, "&gt;") + "</pre>";
+                    $("#" + fileContentPane).append(restxt);
+
+
+                    //parseXML
+                    var text = e.target.result;
+                    parseXMLTree(text, resultpane);
+                    //                    LoadXMLString(resultpane, text);
+
+
+
+                }
+                reader.readAsText(file);
+            }
+
+            // hide load-file div
+            var elementname = "#load-" + resultpane.split("-")[0];
+            $(elementname).slideUp();
+        }
+
+
+    }
 
     // file drag hover
     function fileDragHover(e) {
         e.stopPropagation();
         e.preventDefault();
+        //when a file is dragged over drag-area, change the class of the div->change css
         e.target.className = (e.type == "dragover" ? "file-drag-hover" : "file-drag");
     }
 
-
-
-    // output file information
-    function parseFile(file, resultpane) {
-        var res = "<p>File name: <strong>" + file.name + "</strong><br> type: <strong>" + file.type + "</strong><br> size: <strong>" + file.size + "</strong> bytes</p>";
-        appendToResult(res, resultpane);
-
-        // display text
-        if (file.type.indexOf("text") == 0) {
-            var reader = new FileReader();
-            reader.onload = function (e) {
-                var restxt = "<p><strong>File content:</strong></p><pre>" + e.target.result.replace(/</g, "&lt;").replace(/>/g, "&gt;") + "</pre>";
-                appendToResult(restxt, resultpane);
-            }
-            reader.readAsText(file);
-        }
-
-        // hide load-file div
-        var elementname = "#load-" + resultpane.split("-")[0];
-        $(elementname).slideUp();
-
-    }
 
     // file selection
     function fileSelectHandler(e, result) {
@@ -80,8 +138,8 @@
 
     // call initialization file
     if (window.File && window.FileList && window.FileReader) {
-        init("input-file-select", "input-file-drag", "input-file-content");
-        init("output-file-select", "output-file-drag", "output-file-content");
+        init("input-file-select", "input-file-drag", "input-structure");
+        init("output-file-select", "output-file-drag", "output-structure");
     }
 
 })();
